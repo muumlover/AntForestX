699f8208dc47f453d758ac41e14e386222f64a47
Skogen <muumlover@live.com>
2021-01-08 21:51:21 +0800

    opencv & screenshot

--- app/src/main/java/com/muumlover/antforestx/AntService.kt
+++ app/src/main/java/com/muumlover/antforestx/AntService.kt
@@ -2,12 +2,26 @@ package com.muumlover.antforestx
 
 import android.accessibilityservice.AccessibilityService
 import android.accessibilityservice.GestureDescription
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.Path
 import android.graphics.Rect
+import android.net.Uri
 import android.util.Log
 import android.view.accessibility.AccessibilityEvent
 import android.view.accessibility.AccessibilityNodeInfo
+import org.opencv.android.BaseLoaderCallback
+import org.opencv.android.LoaderCallbackInterface
+import org.opencv.android.OpenCVLoader
+import org.opencv.android.Utils
+import org.opencv.core.Core.minMaxLoc
+import org.opencv.core.Mat
+import org.opencv.imgcodecs.Imgcodecs
+import org.opencv.imgproc.Imgproc.matchTemplate
+import java.io.FileNotFoundException
+import java.io.InputStream
 import java.util.*
+
 
 fun findNodeById(root: AccessibilityNodeInfo, childName: String): AccessibilityNodeInfo? {
     for (i: Int in 0 until root.childCount) {
@@ -38,6 +52,7 @@ class AntService : AccessibilityService() {
 
     companion object {
         var mService: AntService? = null
+
         /**
          * 辅助功能是否启动
          */
@@ -48,8 +63,45 @@ class AntService : AccessibilityService() {
 
     override fun onServiceConnected() {
         super.onServiceConnected()
-        Log.d(TAG, "onServiceConnected——无障碍服务启动成功")
+
+        Log.d(TAG, "无障碍服务启动成功")
         mService = this
+        if (!OpenCVLoader.initDebug()) {
+            Log.d(
+                "OpenCV",
+                "Internal OpenCV library not found. Using OpenCV Manager for initialization"
+            );
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
+        } else {
+            Log.d("OpenCV", "OpenCV library found inside package. Using it!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
+
+    override fun onInterrupt() {
+        Log.d(TAG, "无障碍服务被中断")
+        mService = null
+//        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "无障碍服务已关闭")
+        mService = null
+    }
+
+    private val mLoaderCallback: BaseLoaderCallback = object : BaseLoaderCallback(this) {
+        override fun onManagerConnected(status: Int) {
+            when (status) {
+                LoaderCallbackInterface.SUCCESS -> {
+                    Log.i("OpenCV", "OpenCV loaded successfully")
+                    var imageMat = Mat()
+                }
+                else -> {
+                    super.onManagerConnected(status)
+                }
+            }
+        }
     }
 
     private val nameList = listOf("蚂蚁森林", "好友排行榜")
@@ -67,8 +119,28 @@ class AntService : AccessibilityService() {
         else this.clickNodeList.clear()
         this.titleNow = h5TvTitle.textOrDesc
         this.clickFinish = false
+        // H5标题不是蚂蚁森林就忽略
         if (!h5TvTitle.textOrDesc.endsWith("蚂蚁森林")) return
         Log.d(TAG, "当前页面为：<${h5TvTitle.textOrDesc}>")
+
+        var stream: InputStream? = null
+        val uri: Uri = Uri.parse("android.resource://" + packageName + "/" + R.raw.ball)
+        try {
+            stream = contentResolver.openInputStream(uri)
+        } catch (e: FileNotFoundException) {
+            e.printStackTrace()
+        }
+        val bmpFactoryOptions: BitmapFactory.Options = BitmapFactory.Options()
+        bmpFactoryOptions.inPreferredConfig = Bitmap.Config.ARGB_8888
+        val bmp: Bitmap = BitmapFactory.decodeStream(stream, null, bmpFactoryOptions)
+        val templ = Mat()
+        Utils.bitmapToMat(bmp, templ)
+
+        val image = Imgcodecs.imread("raw/ball.png")
+        val result = Mat()
+        matchTemplate(templ, templ, result, 0)
+        val location = minMaxLoc(result)
+
         val jBarrierFree: AccessibilityNodeInfo = findNodeById(node, "J_barrier_free") ?: return
         jBarrierFree.refresh()
         Log.d(TAG, "共找到${jBarrierFree.childCount}个节点")
@@ -79,7 +151,11 @@ class AntService : AccessibilityService() {
                 Log.d(TAG, "第 $i 个节点被忽略：<$childText>")
                 continue
             }
-            if ((childText.length >= 2 && childText.subSequence(0, 2) == "收集") || childText == " ") {
+            if ((childText.length >= 2 && childText.subSequence(
+                    0,
+                    20
+                ) == "收集") || childText == " "
+            ) {
                 Log.d(TAG, "第 $i 个节点被选中：<$childText>")
                 val ball: AccessibilityNodeInfo? = jBarrierFree.getChild(i)
                 if (ball != null) this.clickNodeList.add(ball)
@@ -90,18 +166,6 @@ class AntService : AccessibilityService() {
 
 
 //        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun onInterrupt() {
-        Log.d(TAG, "无障碍服务被中断")
-        mService = null
-//        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(TAG, "无障碍服务已关闭")
-        mService = null
     }
 
     private fun clickNodes() {
@@ -134,13 +198,17 @@ class AntService : AccessibilityService() {
             .addStroke(GestureDescription.StrokeDescription(path, 100, 50))
             .build()
         val that = this
-        this.dispatchGesture(gestureDescription, object : AccessibilityService.GestureResultCallback() {
-            override fun onCompleted(gestureDescription: GestureDescription) {
-                super.onCompleted(gestureDescription)
-                Log.d("$TAG Gesture", "点击元素中心成功 X:${rect.centerX()} Y:${rect.centerY()}")
-                that.clickNodeList.remove(node)
-                that.clickNodes()
-            }
-        }, null)
+        this.dispatchGesture(
+            gestureDescription,
+            object : AccessibilityService.GestureResultCallback() {
+                override fun onCompleted(gestureDescription: GestureDescription) {
+                    super.onCompleted(gestureDescription)
+                    Log.d("$TAG Gesture", "点击元素中心成功 X:${rect.centerX()} Y:${rect.centerY()}")
+                    that.clickNodeList.remove(node)
+                    that.clickNodes()
+                }
+            },
+            null
+        )
     }
 }
--- app/src/main/java/com/muumlover/antforestx/MainActivity.kt
+++ app/src/main/java/com/muumlover/antforestx/MainActivity.kt
@@ -1,26 +1,40 @@
 package com.muumlover.antforestx
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.media.projection.MediaProjectionManager
 import android.os.Bundle
 import android.provider.Settings
 import android.support.v7.app.AppCompatActivity
 import android.text.TextUtils
+import android.util.Log
 
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = javaClass.name
+    private val REQUEST_MEDIA_PROJECTION = 1001
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if (!hasServicePermission(applicationContext, AntService::class.java) && !AntService.isStart()) {
+        if (!hasServicePermission(
+                applicationContext,
+                AntService::class.java
+            ) && !AntService.isStart()
+        ) {
             try {
+                Log.d(TAG, "尝试跳转到无障碍设置页面")
                 startActivity(Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS))
             } catch (e: Exception) {
-                startActivity(Intent(Settings.ACTION_SETTINGS))
+                Log.d(TAG, "无法跳转到无障碍设置页面")
                 e.printStackTrace()
+                Log.d(TAG, "跳转到系统设置页面")
+                startActivity(Intent(Settings.ACTION_SETTINGS))
             }
         }
+        requestCapturePermission()
     }
 
     /**
@@ -31,6 +45,7 @@ class MainActivity : AppCompatActivity() {
      * @return boolean
      */
     private fun hasServicePermission(ct: Context, serviceClass: Class<*>): Boolean {
+        Log.d(TAG, "检测无障碍权限是否开启")
         var ok = 0
         try {
             ok = Settings.Secure.getInt(ct.contentResolver, Settings.Secure.ACCESSIBILITY_ENABLED)
@@ -40,17 +55,45 @@ class MainActivity : AppCompatActivity() {
         val ms = TextUtils.SimpleStringSplitter(':')
         if (ok == 1) {
             val settingValue =
-                Settings.Secure.getString(ct.contentResolver, Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES)
+                Settings.Secure.getString(
+                    ct.contentResolver,
+                    Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES
+                )
             if (settingValue != null) {
                 ms.setString(settingValue)
                 while (ms.hasNext()) {
                     val accessibilityService = ms.next()
                     if (accessibilityService.contains(serviceClass.simpleName)) {
+                        Log.d(TAG, "无障碍权限已开启")
                         return true
                     }
                 }
             }
         }
+        Log.d(TAG, "无障碍权限未开启")
         return false
+    }
+
+    private fun requestCapturePermission() {
+        Log.d(TAG, "请求屏幕截图权限")
+        val mediaProjectionManager: MediaProjectionManager =
+            getSystemService(Context.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
+        startActivityForResult(
+            mediaProjectionManager.createScreenCaptureIntent(),
+            REQUEST_MEDIA_PROJECTION
+        )
+    }
+
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int,
+        data: Intent?
+    ) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_MEDIA_PROJECTION) {
+            if (resultCode == Activity.RESULT_OK && data != null) {
+                Log.d(TAG, "已经获取到屏幕截图权限")
+            }
+        }
     }
 }
